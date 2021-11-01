db26c863b6443da6b9b100444c1f9819a7deb950
Charles Desiderio <cjdesi@gmail.com>
2021-11-01 16:08:04 -0500

    Oh damnit I left a console log in

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -3,7 +3,6 @@ import styles from './Section.module.css'
 import draggable from '../images/draggable.svg'
 
 const Section = (props) => {
-  console.log(props)
   return (
     <div className={ props.data.included ?  styles.singleSection : ''} >{ props.data.included ? <img src={draggable} alt="Drag Icon" /> : ''  }  { props.data.title }</div>
   )
