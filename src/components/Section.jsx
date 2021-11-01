17bb48b05c739af7a55e2869047fa3e4c9abe4c7
Charles Desiderio <cjdesi@gmail.com>
2021-11-01 15:52:09 -0500

    Added icons to indicate that a section is draggable

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,6 +1,11 @@
+import styles from './Section.module.css'
+
+import draggable from '../images/draggable.svg'
+
 const Section = (props) => {
+  console.log(props)
   return (
-    <div>{ props.data.title }</div>
+    <div className={ props.data.included ?  styles.singleSection : ''} >{ props.data.included ? <img src={draggable} alt="Drag Icon" /> : ''  }  { props.data.title }</div>
   )
 }
 
