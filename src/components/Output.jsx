c201522bfb9acc1d4a31cd388388effce3cb110f
Charles Desiderio <cjdesi@gmail.com>
2021-11-06 21:36:36 -0500

    Added copy functionality

--- src/components/Output.jsx
+++ src/components/Output.jsx
@@ -1,9 +1,12 @@
+import { useRef } from 'react'
 import ReactMarkdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
 
 import styles from './Output.module.css'
 
 const Output = (props) => {
+
+  let copyTextButtonRef = useRef()
 
   let compiledData = ``
 
@@ -13,13 +16,24 @@ const Output = (props) => {
     }
   })
 
+  const copyText = () => {
+    navigator.clipboard.writeText(compiledData)
+    copyTextButtonRef.current.textContent = 'Copied!'
+    copyTextButtonRef.current.style.backgroundColor = '#FAB003'
+    setTimeout(() => {
+      copyTextButtonRef.current.textContent = 'Copy'
+      copyTextButtonRef.current.style.backgroundColor = 'white'
+    }, 1500)
+  }
+
   return (
     <div className={styles.output}>
+      { props.viewType === 'raw' ? <button ref={copyTextButtonRef} className={styles.copyText} onClick={copyText}>Copy</button> : '' }
       { props.viewType === 'pre' ? (<ReactMarkdown escapeHtml={false} className={styles.markdown} children={compiledData} remarkPlugins={[remarkGfm]} />) : '' }
 
       { props.data.map((item, i) => {
         let replaced = item.text.split(/\n/g).filter((item) => item !== '')
-        return item.included && props.viewType === 'raw' ?  <span key={`raw-${i}`}><br />{replaced.map((item) => <span>{item}<br /><br /></span>)}</span> : ''
+        return item.included && props.viewType === 'raw' ? <span key={`raw-${i}`}><br />{replaced.map((item) => <span>{item}<br /><br /></span>)}</span> : ''
       })}
 
     </div>
